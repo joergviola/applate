2babcff533690d13e876d902b7ab5777e24eb89d
joergviola <joerg@joergviola.de>
2019-04-21 20:08:11 +0200

    Http to login

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -3,6 +3,17 @@
 namespace App\Http\Controllers;
 
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 class UserController extends Controller
 {
+    public function login(Request $request) {
+        $credentials = $request->only('email', 'password');
+        if (!Auth::attempt($credentials)) {
+            return response('Forbidden', 403);
+        }
+        return resonse()->json(Auth::user());
+    }
+}
 
--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,15 +1,41 @@
 
 
-const base = window.location.origin + window.location.pathname + "/api/"
+const base = "http://localhost/gdpr-portal/backend/public"
+const oauth= base + "/oauth"
+const api= base + "/api"
 
 export default {
   loggedIn: function() {
     return !!this.user
   },
-  login: function(username, password) {},
+  login: function(email, password) {
+    const data = {email, password}
+    return fetch(base + '/login',{
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, cors, *same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+        // "Content-Type": "application/x-www-form-urlencoded",
+      },
+      redirect: "follow", // manual, *follow, error
+      referrer: "no-referrer", // no-referrer, *client
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+    })
+      .then(response => {
+        if (response.status==200) {
+          return response.json()
+        }
+        throw {
+          code : response.status,
+          message: response.statusText
+        }
+      })
+  },
   find: function(type, query) {},
   get: function(type, id) {
-    return fetch(base + type + '/' + id)
+    return fetch(api + '/' + type + '/' + id)
       .then(response => response.json())
   },
   create: function(type, item) {},
